db38889edf4220b3cdf284faa248d6d3e2ce1e70
shoestrong <shoestrong.zhang@ly.com>
2019-10-18 10:01:45 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+// ./`rollup.config.js`
+
+import typescript from "rollup-plugin-typescript";
+import sourceMaps from "rollup-plugin-sourcemaps";
+
+export default {
+  input: "./src/main.ts",
+  plugins: [
+    typescript({
+      exclude: "node_modules/**",
+      typescript: require("typescript")
+    }),
+    sourceMaps()
+  ],
+  output: [
+    {
+      format: "cjs",
+      file: "lib/bundle.cjs.js",
+      sourcemap: true
+    },
+    {
+      format: "es",
+      file: "lib/bundle.esm.js",
+      sourcemap: true
+    }
+  ]
+};
+
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+let version: string = "0.0.1";
+
+export default (): string => {
+  return `this version is ${version}`;
+};
